f8d239848a520434da91b4009ac9a2f4a36ac3fd
Lujun Weng <loyweng@163.com>
2016-05-26 17:45:19 +0800

    Finish affline transformation

---
+++ Chapter 1/try_hsv.py
@@ -0,0 +1,9 @@
+import cv2
+
+img = cv2.imread('./images/input.jpg')
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imshow('HSV image', hsv_img)
+cv2.imshow('H channel', hsv_img[:, :, 0])
+cv2.imshow('S channel', hsv_img[:, :, 1])
+cv2.imshow('V channel', hsv_img[:, :, 2])
+cv2.waitKey()
